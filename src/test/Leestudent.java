44f06a918bf85e8e2bb08a90788a05ae95141103
Codebytk <ltk37733956@gmail.com>
2022-11-09 15:48:00 +0900

    이해의 첫번째

---
+++ src/test/Leestudent.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Leestudent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println( " 안녕하십니까?");
+		System.out.println( " 이태경입니다.");
+
+	}
+
+}
