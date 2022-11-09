968b9005e867ee4548d028bcb12bdcbfbdcf169e
Codebytk <ltk37722956@gmail.com>
2022-11-09 15:57:28 +0900

    공부1

--- src/test/Leestudent.java
+++ src/test/Leestudent.java
@@ -4,9 +4,11 @@ public class Leestudent {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println( " 안녕하십니까?");
-		System.out.println( " 이태경입니다.");
-
+		System.out.println(" 안녕하십니까?");
+		System.out.println(" 이태경입니다.");
+		System.out.println(" 저는 24살이고");
+		System.out.println(" 국비지원 코딩으로 취업하기 위해");
+		System.out.println(" 열심히 노력하고 있습니다.");
 	}
 
 }
